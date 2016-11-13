17cc2eaea24b69d0861235ce9e138ed4d34c37b1
polypmer <exorable.ludos@gmail.com>
2016-11-13 12:40:15 -0500

    Added Block struct

--- piece.go
+++ piece.go
@@ -1,10 +1,19 @@
 package main
 
 type Piece struct {
-	index int
-	data  []byte
-	peer  *Peer
-	hash  [20]byte
+	index  int
+	data   []byte
+	blocks []*Block
+	peer   *Peer
+	hash   [20]byte
+}
+
+type Block struct {
+	piece      *Piece
+	offset     int
+	length     int
+	downloaded bool
+	data       []byte
 }
 
 func ParsePieces(pieces []byte) []*Piece {
