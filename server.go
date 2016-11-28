5f150dba3720887c69466862969b5f4757f5047c
polypmer <exorable.ludos@gmail.com>
2016-11-28 15:57:21 -0500

    Having figured out how to use magit yet

---
+++ server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	listener net.Listener
+	laddr    string
+}
+
+func Serve(port int, shutdown <-chan bool) error {
+	var err error
+	server := &Server{laddr: fmt.Sprintf(":%d", port)}
+
+	server.listener, err = net.Listen("tcp", server.laddr)
+	if err != nil {
+		debugger.Fatalf("Fatal, server fails: %s", err)
+	}
+	for {
+
+		conn, err := server.listener.Accept()
+		if err == nil {
+			conn.Write([]byte)
+			//go handleConnection(conn, shutdown)
+		}
+		// TODO: handleError(err)
+	}
+	return err
+}
+
+// download (Torrent, Peers, all your stuff)...
+// protected by mutex, inteface to stuff, goes through mutex.
+// go NewServer(download, port)
